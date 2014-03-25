950d35769d7cc5a9ba8c24f7e5f5913ecd003940
Phillip Wong <wongphil@gmail.com>
2014-03-25 08:53:25 -0700

    Added rhino mocks tests for indexed mocks.

--- MockAllTheThings.RhinoMocks.Tests/RhinoMocksMockProviderTests.cs
+++ MockAllTheThings.RhinoMocks.Tests/RhinoMocksMockProviderTests.cs
@@ -59,6 +59,87 @@ namespace MockAllTheThings.RhinoMocks.Tests
 
 			Assert.AreSame(mockedTestService.TestInterface, mockTestInterface);
 		}
+
+		[Test]
+		public void CanMockTestServiceWithIndexedMock()
+		{
+			var mockTestInterfaceObject = MockRepository.GenerateMock<ITestInterface>();
+
+			var mockedTestService = 
+				Create
+					.MeA<TestServiceWithMixedDependencies>()
+					.UsingThisInstanceToMock<ITestInterface>(mockTestInterfaceObject)
+					.SpecificallyForTheArgumentAt(0)
+					.AndUsingThisInstanceToMock<TestAbstractClass>(MockRepository.GenerateMock<TestAbstractClass>())
+					.AndMockingAllTheOtherThings();
+
+			Assert.IsNotNull(mockedTestService);
+			Assert.IsInstanceOf<TestServiceWithMixedDependencies>(mockedTestService);
+
+			Assert.AreSame(mockedTestService.TestInterface, mockTestInterfaceObject);
+		}
+
+		[Test]
+		public void CanMockTestServiceWithIndexedMockWhenThereAreMultipleDependenciesOfSameInterface_FirstPosition()
+		{
+			var mockTestInterfaceObject = MockRepository.GenerateMock<ITestInterface>();
+
+			var mockedTestService = 
+				Create
+					.MeA<TestServiceWithMultipleIdenticalDependencies>()
+					.UsingThisInstanceToMock<ITestInterface>(mockTestInterfaceObject)
+					.SpecificallyForTheArgumentAt(0)
+					.AndMockingAllTheOtherThings();
+
+			Assert.IsNotNull(mockedTestService);
+			Assert.IsInstanceOf<TestServiceWithMultipleIdenticalDependencies>(mockedTestService);
+
+			Assert.AreSame(mockedTestService.TestInterface1, mockTestInterfaceObject);
+			Assert.AreNotSame(mockedTestService.TestInterface2, mockTestInterfaceObject);
+			Assert.AreNotSame(mockedTestService.TestInterface3, mockTestInterfaceObject);
+			Assert.AreNotSame(mockedTestService.TestInterface2, mockedTestService.TestInterface3);
+		}
+
+		[Test]
+		public void CanMockTestServiceWithIndexedMockWhenThereAreMultipleDependenciesOfSameInterface_LastPosition()
+		{
+			var mockTestInterfaceObject = MockRepository.GenerateMock<ITestInterface>();
+
+			var mockedTestService = 
+				Create
+					.MeA<TestServiceWithMultipleIdenticalDependencies>()
+					.UsingThisInstanceToMock<ITestInterface>(mockTestInterfaceObject)
+					.SpecificallyForTheArgumentAt(2)
+					.AndMockingAllTheOtherThings();
+
+			Assert.IsNotNull(mockedTestService);
+			Assert.IsInstanceOf<TestServiceWithMultipleIdenticalDependencies>(mockedTestService);
+
+			Assert.AreSame(mockedTestService.TestInterface3, mockTestInterfaceObject);
+			Assert.AreNotSame(mockedTestService.TestInterface1, mockTestInterfaceObject);
+			Assert.AreNotSame(mockedTestService.TestInterface2, mockTestInterfaceObject);
+			Assert.AreNotSame(mockedTestService.TestInterface1, mockedTestService.TestInterface2);
+		}
+
+		[Test]
+		public void CanMockTestServiceWithTypeMocksWhenThereAreMultipleDependenciesOfSameInterface()
+		{
+			var mockTestInterfaceObject = MockRepository.GenerateMock<ITestInterface>();
+
+			var mockedTestService = 
+				Create
+					.MeA<TestServiceWithMultipleIdenticalDependencies>()
+					.UsingThisInstanceToMock<ITestInterface>(mockTestInterfaceObject)
+					.AndMockingAllTheOtherThings();
+
+			Assert.IsNotNull(mockedTestService);
+			Assert.IsInstanceOf<TestServiceWithMultipleIdenticalDependencies>(mockedTestService);
+
+			Assert.AreSame(mockedTestService.TestInterface1, mockTestInterfaceObject);
+			Assert.AreSame(mockedTestService.TestInterface2, mockTestInterfaceObject);
+			Assert.AreSame(mockedTestService.TestInterface3, mockTestInterfaceObject);
+			Assert.AreSame(mockedTestService.TestInterface2, mockedTestService.TestInterface3);
+		}
 	}
 }
 
